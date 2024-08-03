5f037d8450d79d8fd1e232881c445b20d0ea8a8a
gdav1138 <gdav1138@gmail.com>
2024-08-02 19:55:28 -0700

    Import requests to get json function to pull quote from forbes website. Commented out send_message variable, was throwing an error (not sure if I changed something incorrectly there by accident).

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import zmq
+import requests
+
+## Quote of the day - url "https://quotes.rest/qod?language=en"
 
 def server(ip: str = 'localhost', port: int = 6969):
     context = zmq.Context()
@@ -12,13 +15,16 @@ def server(ip: str = 'localhost', port: int = 6969):
         print(f"{svc_string}: message from client - {message}")
 
         # **** add your server logic here ****
-        response = "YOUR QUOTE HERE"
-        # **** add your server logic here ^^^^ ****
+        response = requests.get("http://www.forbes.com/forbesapi/thought/uri.json?enrich=true&query=1&relatedlimit=5")
+        data = response.json()
 
-        send_message = response.encode('utf-8')
+        quote_of_the_day = data['thought']["quote"]
+        socket.send_string(quote_of_the_day)
+        # **** add your server logic here ^^^^ ****
+        # send_message = response.encode('utf-8')
         new_message = f"{svc_string}: sending response to client - '{response}'"
         print(new_message)
-        socket.send(send_message)
+        # socket.send(send_message)
 
 if __name__ == '__main__':
     server()
